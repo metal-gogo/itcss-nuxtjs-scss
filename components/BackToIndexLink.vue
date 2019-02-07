00ff0edb44158dcacafdeff03c48acd90dcd3991
metal-gogo <daniel@beek.io>
2019-02-07 02:07:35 -0600

    ⭐️ add BackToIndexLink component

---
+++ components/BackToIndexLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <nuxt-link to="/" class="back-to-index-link">🔙 Regresar al índice</nuxt-link>
+</template>
+
+<!-- Using the `scoped` attribute -->
+<style lang="scss" scoped>
+@import '~assets/styles/settings/variables';
+@import '~assets/styles/tools/mixins';
+
+.back-to-index-link {
+  color: $accent-100;
+  border-bottom: 2px solid $primary-35;
+  transition: border-bottom 0.3s ease-in-out;
+
+  &:hover,
+  &:focus {
+    color: $accent-100;
+    text-decoration: none;
+    border-bottom: 2px solid $primary-100;
+  }
+}
+</style>
