7c6b1ebe96cb3b9db639667f4818335641455c20
metal-gogo <daniel@beek.io>
2019-02-06 22:48:51 -0600

    🐣 initial commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>ITCSS with Nuxt and SCSS</h1>
+      <h2>Base project using ITCSS with Nuxt.js and SCSS</h2>
+    </div>
+  </section>
+</template>
